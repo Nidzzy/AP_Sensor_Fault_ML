1ee578bc54992572e37266b87e8f9e2096e59949
Nidzzy <shaikhn789@gmail.com>
2023-01-25 20:22:20 +0530

    Updated requirement list

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages,setup
+from typing import List
+
+REQUIREMENT_FILE_NAME = "requirements.txt"
+
+
+def get_requirements()->List[str]:
+    with open(REQUIREMENT_FILE_NAME) as requiremnt_file:
+        requiremnt_list = requiremnt_file.readlines()
+        requiremnt_list = [requirement_name.replace("\n", "") for requirement_name in requiremnt_list]
+
+
+setup(
+    name = "sensor",
+    version = "0.0.0.1",
+    author = " Nidzzy ",
+    author_email= "shaikhn789@gmail.com",
+    packages = find_packages(),
+    install_requires = get_requirements(),
+
+    )
