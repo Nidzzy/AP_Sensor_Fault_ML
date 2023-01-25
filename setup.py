ddcd477067bfc24f319afecf17bdef65cf14f28d
Nidzzy <shaikhn789@gmail.com>
2023-01-25 20:32:14 +0530

    Updated requirement list

--- setup.py
+++ setup.py
@@ -7,8 +7,12 @@ REQUIREMENT_FILE_NAME = "requirements.txt"
 def get_requirements()->List[str]:
     with open(REQUIREMENT_FILE_NAME) as requiremnt_file:
         requiremnt_list = requiremnt_file.readlines()
-        requiremnt_list = [requirement_name.replace("\n", "") for requirement_name in requiremnt_list]
-
+     
+    requirement_list = [requirement_name.replace("\n", "") for requirement_name in requirement_list]
+    
+    if HYPHEN_E_DOT in requirement_list:
+        requirement_list.remove(HYPHEN_E_DOT)
+    return requirement_list
 
 setup(
     name = "sensor",
